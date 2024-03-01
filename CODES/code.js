02e052c61106a0059ba9c99a2efc21cf7e26635b
moureenmusimbi <musimbimoureen4@gmail.com>
2024-03-01 22:02:35 +0300

    my commite

---
+++ CODES/code.js
@@ -0,0 +1,9 @@
+console.log("My javascript code")
+
+
+// declarition and assignment of variables
+let my_age = 20;
+let my_name = "Jane"
+
+console.log("My name is :", my_name)
+console.log("I am " + my_age + " years old")
---
+++ javascript-projects/script/script.js
@@ -0,0 +1,14 @@
+//how to accept user input
+//1. easy way= window prompt
+//2. professional way = html textbox
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(x, typeof y);
+console.log(x, typeof z);
